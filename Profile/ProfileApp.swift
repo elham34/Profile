44eff5f3eb213496a207465ff3d7c74e15894316
amalabughazaleh91 <amal_adel91@hotmail.com>
2023-10-08 11:38:15 +0300

    Initial Commit

---
+++ Profile/ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileApp.swift
+//  Profile
+//
+//  Created by Amal Abughazaleh on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
